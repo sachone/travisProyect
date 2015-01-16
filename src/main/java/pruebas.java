1a411e3901002a1311f8ea487e42b3d2253d21d5
al037542 <al037542@s09-pc16>
2015-01-16 17:51:32 +0100

    primer commit

---
+++ src/main/java/pruebas.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author al037542
+ */
+public class pruebas {
+    public pruebas(){
+        
+    }
+    
+    public Integer multiplicacion(Integer numero1, Integer numero2){
+        return numero1 * numero2;
+    }
+}
